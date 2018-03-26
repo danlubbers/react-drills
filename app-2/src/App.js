e34ab305b87eb5a880625468ba529d29ff65f1df
Dan Lubbers <lubbers.dan@gmail.com>
2018-03-26 17:35:05 -0600

    app-2-drill-completed

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -3,16 +3,26 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state={
+      gear: ['backpack', 'rain jacket', 'trekking pole', 'water purifier']
+    }
+  }
+
+
   render() {
+    let gearToDisplay = this.state.gear.map((element, index) => {
+      return (
+        <h2 key={index}>{element}</h2>
+      )
+    })
+    
+
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+    <p>{gearToDisplay}</p>
       </div>
     );
   }
