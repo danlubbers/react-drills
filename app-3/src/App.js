38d93c436b48d1592c476f9dd9ba8e5c438c4d12
Dan Lubbers <lubbers.dan@gmail.com>
2018-03-26 19:31:51 -0600

    app-3-drill-completed

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -3,16 +3,29 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state={
+      filterString: [],
+      gear: ['backpack', 'rain jacket', 'trekking pole', 'water purifier']
+    }
+  }
+
+  handleChange(val) {
+    this.setState({filterString: val})
+  }
+
   render() {
+    let gearToDisplay = this.state.gear.filter((e, i)=>{
+      return e.includes( this.state.filterString);
+    }).map((e, i)=>{
+      return <h2 key={i}>{e}</h2>
+    })
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <input onChange={(e)=>this.handleChange(e.target.value)}/>
+      <h2>{gearToDisplay}</h2>
       </div>
     );
   }
